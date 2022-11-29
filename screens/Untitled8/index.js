1a0a3026f1d320fc3c6409fab781dc116880252e
Aline Tavares <aline@crowdbotics.com>
2022-11-29 16:48:23 -0500

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled8/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled8 = () => {
+  return <View style={_styles.XQpblMpO}>
+      </View>;
+};
+
+export default Untitled8;
+
+const _styles = StyleSheet.create({
+  XQpblMpO: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
---
+++ screens/Untitled9/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled9 = () => {
+  return <View style={_styles.ekUpMMxd}>
+      </View>;
+};
+
+export default Untitled9;
+
+const _styles = StyleSheet.create({
+  ekUpMMxd: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
