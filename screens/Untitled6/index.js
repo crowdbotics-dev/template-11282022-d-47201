7a3208e59a633f8c0a8f5555f494cf5d5c809ff0
Aline Tavares <aline@crowdbotics.com>
2022-11-29 14:54:26 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled6/index.js
+++ screens/Untitled6/index.js
@@ -3,7 +3,7 @@ import { View, StyleSheet } from "react-native";
 
 const Untitled6 = () => {
   return <View style={_styles.LbKLmutg}>
-      </View>;
+      <View style={_styles.pujQkoUb}></View></View>;
 };
 
 export default Untitled6;
@@ -14,5 +14,12 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  pujQkoUb: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
